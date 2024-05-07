148f2ecd37134f9631784b12a3a72e2fa33a2c72
renad-az <reenad.abdulazizz@gmail.com>
2024-05-07 22:53:06 +0300

    model file

--- QuoteProject/apps/quotemodule/models.py
+++ QuoteProject/apps/quotemodule/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+
 class Quote(models.Model):
+    Tilte = models.CharField(max_length=100)
     text = models.TextField()
     author = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
