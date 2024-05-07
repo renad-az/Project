cb3ec2f821c774adf3a4d72f747987fba2fc7651
renad-az <reenad.abdulazizz@gmail.com>
2024-05-07 22:57:54 +0300

    new update

--- QuoteProject/main/settings.py
+++ QuoteProject/main/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.bookmodule',
+    'apps.quotemodule',
     'apps.usermodule',
 ]
 
