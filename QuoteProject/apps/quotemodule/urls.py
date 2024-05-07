b92d7f0eab9195360fd48d643dd576b9112a5bef
renad-az <reenad.abdulazizz@gmail.com>
2024-05-07 22:53:28 +0300

    update url

--- QuoteProject/apps/quotemodule/urls.py
+++ QuoteProject/apps/quotemodule/urls.py
@@ -3,6 +3,7 @@ from . import views
 urlpatterns = [
     path('', views.quote, name='quote'),
     path('add/', views.add_quote, name='add_quote'),
-    path('quote/<int:quote_id>/', views.quote_detail, name='quote_detail'),
-    path('profile/', views.profile, name='profile'),
+    path('quote_detail/<int:qId>/', views.quote_detail, name='quote_detail'),
+
+    path('Search/', views.search, name='search'),
 ]
